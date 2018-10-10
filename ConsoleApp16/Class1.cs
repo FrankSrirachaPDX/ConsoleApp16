224acfb5d77a5878b201329c8f470375a18d01de
Ryan <werner.ryan18@gmail.com>
2018-10-10 14:57:46 -0700

    Add project files.

---
+++ ConsoleApp16/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    public static class Class1
+    {
+        public static void Math(int x ,out int number)
+        {
+
+            number = x / 2;
+
+        }
+        public static void Math(out double number)
+        {
+            number = 44.4;
+        }
+
+    }
+}
---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 2;
+            double dub;
+            string temp;
+
+            Console.WriteLine("Enter a number to be halved");
+            temp = Console.ReadLine();
+            num = Convert.ToInt16(temp);
+
+
+            Class1.Math(num, out num);
+            Console.WriteLine(num);
+
+            Class1.Math(out dub);
+
+            Console.WriteLine(dub);
+
+            Console.ReadLine();
+
+        }
+    }
+}
